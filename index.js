fbf59a0ee4541a3e79a6c6a9d62825bd4779a360
Trang Le <trang.lt157@gmail.com>
2022-11-02 11:51:11 -0700

    fetch calls

---
+++ index.js
@@ -0,0 +1,25 @@
+//import { use } from 'passport';
+//import express from 'express';
+//import OAuth2Strategy from 'passport-oauth2/lib';
+
+/* use(new OAuth2Strategy({
+    authorizationURL: 'https://api.genius.com/oauth/authorize',
+    tokenURL: 'https://api.genius.com/oauth/token',
+    clientID: "UNhvJ3Gdn16ZrYENlkP84rN7P9-3sTHKaWUfV9Qq6UO9JcZ661k7iQpxT_AhCkwh",
+    clientSecret: 'iMuxrrD1xtTFBjzEltan74BwVXSNxx_mo42E8wqdLsr1dcayrhR7mSiucy6MlNgyW90ID7xcc8f7fs24kmqzsA',
+    callbackURL: 'https://trangle157.github.io/iu-lyrics-viz/',
+    scope: 'me',
+    state: 'hug'
+},
+    function (accessToken, refreshToken, profile, cb) {
+        User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+            return cb(err, user);
+        });
+    }
+)
+)
+ */
+//console.log("passport", passport);
+
+const bearer = '1wQro5cHjKZp1pQpLNazyZe2w-3bRNwRFnbQvLnU5Pio6AN1Yc2E2EK4RnU2AG5i';
+
